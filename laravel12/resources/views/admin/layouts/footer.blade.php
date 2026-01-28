a3f495e2a89e93f05633bc246f0bb84ef9bb1b57
Tapan Das <tpnds123@gmail.com>
2026-01-28 18:40:29 +0530

    Admin Panel Layout Set

--- laravel12/resources/views/admin/layouts/footer.blade.php
+++ laravel12/resources/views/admin/layouts/footer.blade.php
@@ -1,21 +1,12 @@
-<footer class="main-footer d-flex justify-content-between align-items-center">
-
-    <!-- LEFT -->
-    <div class="footer-left">
-        <strong>
-            © {{ date('Y') }}
-            <a href="{{ config('app.url') }}" target="_blank">
-                {{ config('app.name', 'Admin Panel') }}
-            </a>
-        </strong>
-        <span class="ml-1">All rights reserved.</span>
-    </div>
-
-    <!-- RIGHT -->
-    <div class="footer-right d-none d-sm-inline-block">
-        <span class="text-muted">
-            Version {{ config('app.version', '1.0.0') }}
-        </span>
-    </div>
-
+<footer class="app-footer">
+    <!--begin::To the end-->
+    <div class="float-end d-none d-sm-inline">Anything you want</div>
+    <!--end::To the end-->
+    <!--begin::Copyright-->
+    <strong>
+        Copyright &copy; {{ date('Y') }}&nbsp;
+        <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
+    </strong>
+    All rights reserved.
+    <!--end::Copyright-->
 </footer>
